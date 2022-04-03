c38ff3ed95667d4448ec70c7eb49bd032824e9e0
Johann Erhard <johann.erhard@tum.de>
2022-04-04 01:05:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='sensor_async',
     description='Sensor library for asynchronous reading using thread- or asyncio-workflow.',
+    project_urls={
+        'GitHub': 'https://github.com/Etbelo/SensorAsync'
+    },
+    license='MIT',
+    packages=find_packages(include=['sensor_async']),
     author='Johann Erhard',
     zip_safe=False,
     install_requires=[
